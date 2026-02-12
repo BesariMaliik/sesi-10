c505f6834162991bd63410288bcfe824bd2a21b1
BesariMaliik <muhammadmalik611@gmail.com>
2026-02-12 15:03:01 +0700

    homework sesi-9 Web Automation

---
+++ cobatest/cobaAja.js
@@ -0,0 +1,83 @@
+import { Builder, By, until } from "selenium-webdriver";
+import { expect } from "chai";
+
+describe("SauceDemo Automation Test", function () {
+  let driver;
+  this.timeout(40000);
+
+  before(async () => {
+    driver = await new Builder().forBrowser("chrome").build();
+  });
+
+  // =========================
+  // TEST CASE 1 - LOGIN
+  // =========================
+  it("TC01 - Login berhasil menggunakan user valid", async () => {
+    await driver.get("https://www.saucedemo.com/");
+
+    let inputUsername = await driver.findElement(By.css('[data-test="username"]'))
+       await inputUsername.sendKeys('standard_user');
+
+     let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]'))
+      await inputPassword.sendKeys('secret_sauce');
+
+     let buttonLogin = await driver.findElement(By.className('submit-button btn_action'))
+      await buttonLogin.click();
+
+    await driver.wait(until.urlContains("inventory.html"), 10000);
+
+    const title = await driver
+      .findElement(By.className("title"))
+      .getText()
+      
+    expect(title).to.equal("Products");
+  });
+
+  // =========================
+  // TEST CASE 2 - SORT Z → A
+  // =========================
+  it("TC02 - Mengurutkan produk dari Z ke A", async () => {
+    const sortDropdown = await driver.findElement(
+      By.xpath('//select[@data-test="product-sort-container"]'));
+      await sortDropdown.sendKeys("Name (Z to A)");
+
+    const productElements = await driver.findElements(
+      By.className("inventory_item_name")
+    );
+
+    const productNames = [];
+    for (let el of productElements) {
+      productNames.push(await el.getText());
+    }
+
+    const expectedSorted = [...productNames].sort().reverse();
+
+    expect(productNames).to.deep.equal(expectedSorted);
+
+  });
+
+  it("TC03 - Mengurutkan produk berdasarkan harga Low ke High", async () => {
+    const sortDropdown = await driver.findElement(
+       By.className("product_sort_container")
+      );
+      await sortDropdown.sendKeys("Price (low to high)");
+
+    const priceElements = await driver.findElements(
+      By.className("inventory_item_price")
+    );
+
+    const prices = [];
+    for (let price of priceElements) {
+      prices.push(parseFloat((await price.getText()).replace("$", "")));
+    }
+
+    const expectedSortedPrices = [...prices].sort((a, b) => a - b);
+
+    expect(prices).to.deep.equal(expectedSortedPrices);
+  });
+
+  after(async () => {
+    await driver.quit();
+  });
+
+});
